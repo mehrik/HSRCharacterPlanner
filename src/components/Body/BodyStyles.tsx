e6b204d4a71b4b6c895aece31b28ba535ca7ac5b
maric.sobreo <mehrik.dev@gmail.com>
2023-06-01 14:53:16 -0700

    add eslint

--- src/components/Body/BodyStyles.tsx
+++ src/components/Body/BodyStyles.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
-import background from '../../resources/background.png';
-
 export const Wrapper = styled.div`
   padding: 64px 53px 53px;
   height: calc(100% - 184px); // 67px + (53px * 2) === Header + padding

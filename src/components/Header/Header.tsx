275771e151099342c6c499507a93cff108f8e6c2
maric.sobreo <mehrik.dev@gmail.com>
2023-06-01 12:38:36 -0700

    add headaer and styled-components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from "./HeaderStyles";
+
+export const Header = () => {
+  return (
+    <Wrapper>
+      <h1>
+        Honkai: Star Rail - Character Planner
+      </h1>
+    </Wrapper>
+  )
+}
---
+++ src/components/Header/HeaderStyles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 67px;
+  background: #4e4450;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

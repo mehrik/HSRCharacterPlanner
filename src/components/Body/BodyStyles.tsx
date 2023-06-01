42ce9dbd4a79545e9a7e219fe13e7bb89a975f18
maric.sobreo <mehrik.dev@gmail.com>
2023-06-01 13:17:27 -0700

    fix layout padding

--- src/components/Body/BodyStyles.tsx
+++ src/components/Body/BodyStyles.tsx
@@ -4,4 +4,5 @@ import background from '../../resources/background.png';
 
 export const Wrapper = styled.div`
   padding: 64px 53px 53px;
+  height: calc(100% - 184px); // 67px + (53px * 2) === Header + padding
 `;

394ba0192172bc6096d8592785d04b6de8f210c6
maric.sobreo <mehrik.dev@gmail.com>
2023-06-01 16:22:31 -0700

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "./App.css";
 import { Layout } from "./components/Layout/Layout";
 import { Header } from "./components/Header/Header";
 import { Body } from "./components/Body/Body";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Header />
-        <Body />
-      </Layout>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <Layout>
+          <Header />
+          <Body />
+        </Layout>
+      </div>
+    </ThemeProvider>
   );
 }
 
